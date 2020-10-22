e3afe9975649e564a423cff6643c12ecf2d876d6
Evan Fiordeliso <evan.fiordeliso@gmail.com>
2020-10-22 17:45:57 -0400

    Rename PlayerJoinedMessage to PlayerJoinMessage

--- app/src/main/java/edu/temple/dmhelper/bluetooth/message/PlayerJoinMessage.java
+++ app/src/main/java/edu/temple/dmhelper/bluetooth/message/PlayerJoinMessage.java
@@ -2,12 +2,12 @@ package edu.temple.dmhelper.bluetooth.message;
 
 import edu.temple.dmhelper.Character;
 
-public class PlayerJoinedMessage extends Message {
+public class PlayerJoinMessage extends Message {
     public static final String TYPE = "player_joined";
 
     private final Character player;
 
-    public PlayerJoinedMessage(Character player) {
+    public PlayerJoinMessage(Character player) {
         super(TYPE);
         this.player = player;
     }
