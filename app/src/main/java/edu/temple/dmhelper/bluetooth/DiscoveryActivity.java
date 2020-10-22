f07eaf4ea60777f6660ed2779f34ff6f799f039c
Evan Fiordeliso <evan.fiordeliso@gmail.com>
2020-10-22 16:11:01 -0400

    Add DiscoveryActivity

---
+++ app/src/main/java/edu/temple/dmhelper/bluetooth/DiscoveryActivity.java
@@ -0,0 +1,16 @@
+package edu.temple.dmhelper.bluetooth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.temple.dmhelper.R;
+
+public class DiscoveryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_discovery);
+    }
+}
