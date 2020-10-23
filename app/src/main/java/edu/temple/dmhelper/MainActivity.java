31ab1b88bbc3d3da8e76b7ff2465a20a92aa8ea3
Evan Fiordeliso <evan.fiordeliso@gmail.com>
2020-10-23 13:18:45 -0400

    Add discovery activity launching
    Added handling for starting the discovery activity and geting the result.

--- app/src/main/java/edu/temple/dmhelper/MainActivity.java
+++ app/src/main/java/edu/temple/dmhelper/MainActivity.java
@@ -1,14 +1,47 @@
 package edu.temple.dmhelper;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
+import edu.temple.dmhelper.bluetooth.DiscoveryActivity;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = "MainActivity";
+
+    public static final int REQUEST_DISCOVER_DEVICE = 1001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void startDiscoveryActivity() {
+        Log.d(TAG, "Launching Discovery Activity");
+        Intent intent = new Intent(this, DiscoveryActivity.class);
+        startActivityForResult(intent, REQUEST_DISCOVER_DEVICE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == REQUEST_DISCOVER_DEVICE) {
+            if (resultCode == RESULT_OK && data != null) {
+                BluetoothDevice device = (BluetoothDevice) data.getParcelableExtra(DiscoveryActivity.EXTRA_DEVICE);
+                if (device != null) {
+                    Log.d(TAG, "Device selected: " + device.toString());
+                    // TODO: Handle selected device
+                }
+            } else {
+                Log.d(TAG, "Discovery cancelled");
+                // TODO: Handle discovery cancelled
+            }
+        }
     }
 }
