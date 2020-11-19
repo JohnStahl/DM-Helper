eeb18e077eea7805f95eecafcbece4ced737ccfe
John Stahl <tug89585@temple.edu>
2020-11-18 23:06:21 -0500

    Implemented CharacterAdapter.

--- app/src/main/java/edu/temple/dmhelper/CharacterAdapter.java
+++ app/src/main/java/edu/temple/dmhelper/CharacterAdapter.java
@@ -1,9 +1,12 @@
 package edu.temple.dmhelper;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class CharacterAdapter extends BaseAdapter {
     Context context;
@@ -31,6 +34,15 @@ public class CharacterAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        TextView nameTextView;
+
+        if (!(convertView instanceof LinearLayout)){
+            convertView = LayoutInflater.from(context).inflate(R.layout.character_adapter_layout, parent, false);
+        }
+
+        nameTextView = convertView.findViewById(R.id.nameTextView);
+        nameTextView.setText(((Character) getItem(position)).getName());
+        
+        return convertView;
     }
 }
